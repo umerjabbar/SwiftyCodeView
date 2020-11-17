ec5629f126ecd42e1763e1ebb9f0ec901202cfc8
Umer Jabbar <ujabbar@wewanttraffic.pk>
2020-11-17 17:36:33 +0500

    changes

--- SwiftyCodeView/Classes/SwiftyCodeView/SwiftyCodeItemView.swift
+++ SwiftyCodeView/Classes/SwiftyCodeView/SwiftyCodeItemView.swift
@@ -10,6 +10,8 @@ open class SwiftyCodeItemView: UIView {
 
     @IBOutlet open weak var textField: SwiftyCodeTextField!
     
+    var bottomLine = CALayer()
+    
     override open func awakeFromNib() {
         super.awakeFromNib()
         textField.text = ""
@@ -19,12 +21,27 @@ open class SwiftyCodeItemView: UIView {
     }
     
     private func applyBorderedShadow() {
-        layer.shadowOpacity = 1
-        layer.shadowColor = UIColor(red: 227/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1).cgColor
-        layer.shadowOffset = CGSize(width: 0, height: 1)
-        layer.shadowRadius = 8
-        layer.masksToBounds = false
-        layer.cornerRadius = 4
+//        layer.shadowOpacity = 1
+//        layer.shadowColor = UIColor(red: 227/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1).cgColor
+//        layer.shadowOffset = CGSize(width: 0, height: 1)
+//        layer.shadowRadius = 8
+//        layer.masksToBounds = false
+//        layer.cornerRadius = 4
+        self.backgroundColor = .clear
+        
+        
+        
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        bottomLine.removeFromSuperlayer()
+        bottomLine.frame = CGRect(x: 0.0, y: frame.height - 2, width: frame.width, height: 4.0)
+        bottomLine.backgroundColor = UIColor.darkGray.cgColor
+        bottomLine.cornerRadius = 2
+        bottomLine.masksToBounds = true
+        layer.addSublayer(bottomLine)
+        
     }
     
     override open func becomeFirstResponder() -> Bool {
--- SwiftyCodeView/Classes/SwiftyCodeView/SwiftyCodeTextField.swift
+++ SwiftyCodeView/Classes/SwiftyCodeView/SwiftyCodeTextField.swift
@@ -7,16 +7,25 @@
 import UIKit
 
 public protocol SwiftyCodeTextFieldDelegate: class {
-	func deleteBackward(sender: SwiftyCodeTextField, prevValue: String?)
+    func deleteBackward(sender: SwiftyCodeTextField, prevValue: String?)
 }
 
 open class SwiftyCodeTextField: UITextField {
     
     weak open var deleteDelegate: SwiftyCodeTextFieldDelegate?
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.textColor = UIColor.darkGray
+        self.tintColor = UIColor.darkGray
+        self.font = UIFont.boldSystemFont(ofSize: 30)
+        self.backgroundColor = .clear
+    }
 
     override open func deleteBackward() {
-		let prevValue = text
-		super.deleteBackward()
+        let prevValue = text
+        super.deleteBackward()
         deleteDelegate?.deleteBackward(sender: self, prevValue: prevValue)
     }
 }
